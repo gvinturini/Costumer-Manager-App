d529df5ac5ccc02da282e62226298101565df79f
Giovanni Vinturini <go_vinturini@hotmail.com>
2024-03-17 19:55:51 -0300

    Add project files.

---
+++ Costumer Manager/Data/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Costumer_Manager.Data.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Costumer_Manager.Data;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<CustomerModel> Customers { get; set; }
+}
---
+++ Costumer Manager/Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Costumer_Manager.Data.DataModels;
+
+namespace Costumer_Manager.Models;
+
+public class HomeViewModel
+{
+    public List<CustomerModel> CustomerList { get; set; }
+}
